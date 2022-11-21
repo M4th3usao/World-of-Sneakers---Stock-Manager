2a25645b531d834192b9b55b05dcab27544cf2cd
Matheus Henrique <matheus.rico17@gmail.com>
2022-11-21 11:47:42 -0300

    home and footer contente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { AccountComponent } from './components/pages/account/account.component';
+import { CatalogComponent } from './components/pages/catalog/catalog.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'catalogo', component: CatalogComponent},
+  {path: 'account', component: AccountComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
